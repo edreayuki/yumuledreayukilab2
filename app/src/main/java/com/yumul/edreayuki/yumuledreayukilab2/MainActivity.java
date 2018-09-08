e8730ef91c1d016e381ef2a555cb76797b62729b
Yuki <edreayuki.yumul.iics@ust.edu.ph>
2018-09-08 18:12:47 +0800

    Initial commit

---
+++ app/src/main/java/com/yumul/edreayuki/yumuledreayukilab2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yumul.edreayuki.yumuledreayukilab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.yumul.edreayuki.yumuledreayukilab2.R.layout.screen1);
+    }
+}
